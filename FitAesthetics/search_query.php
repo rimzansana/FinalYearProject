60d0e6e29a04b4d616133b66cba1904cee3adb15
Rimzan Sadikeen <rimzansadikeen@ymail.com>
2018-04-20 13:19:10 +0530

    Search Query Update
    - Added General loading sql in else statements.

--- FitAesthetics/search_query.php
+++ FitAesthetics/search_query.php
@@ -85,12 +85,87 @@ if(isset($_POST['search-button'])){
     echo '</script>';
 
 
+          $sql = "SELECT * FROM trainer";
+          $result = mysqli_query($dbcon,$sql);
+          $queryResults = mysqli_num_rows($result);
+
+          if($queryResults>0){
+
+            echo "<div class='container'>
+              <div class='row container'>
+                <h5><p>We have found</p></h5>
+                <h5><p> &nbsp;".$queryResults."</p></h5>
+                <h5><p> &nbsp;results</p></h5>
+              </div>
+            </div>";
+
+              while($row= mysqli_fetch_assoc($result)){
+
+
+
+                echo "<div class='col-md-6 pt-3'>
+                  <div class='card flex-md-row mb-4 box-shadow h-md-250'>
+                          <img class='card-img-right flex-auto d-none d-md-block h-100' src='".$row['image']."' alt='Card image cap' width='160' />
+                          <div class='card-body d-flex flex-column align-items-start'>
+
+                              <h3 class='mb-0 container'>
+                              <div class='row'>
+                              <a class='text-dark'>".$row['fName']."</a>
+                              <a class='text-dark'>&nbsp;".$row['lName']."</a>
+                              </div>
+
+
+                              </h3>
+
+                          <div class='text-muted'>".$row['location']."</div>
+
+                          <div class='container'>
+                              <div class='row my-3'>
+                              <p> Experience : </p>
+                              <p> &nbsp; ".$row['experience']."</p>
+                              </div>
+
+                              <div class='row mb-auto'>
+                              <p>".$row['fee']."</p>
+                              <p> &nbsp;LKR per month</p>
+                              </div>
+                          </div>
+
+                          <div class='w-100'>
+
+                              <div class='row'>
+                                  <div class='col w-50'>
+                                  <a href='trainerBio.php?firstname=".$row['fName']."&lastname=".$row['lName']."' id='text-decoration-none'>View Profile</a>
+                                  </div>
+
+                                  <div class='col d-flex justify-content-end'>
+                                  *****
+                                  </div>
+
+                              </div>
+                          </div>
+
+                          </div>
+
+                      </div>
+
+                  </div>";
+
+
+                }
+          }
+
+
+
+    }
+
+
 
   }
 
 
 
-}
+
 
 
 /* Search criteria for the Index page */
@@ -175,12 +250,87 @@ else if(isset($_POST['index-search'])){
     echo '</script>';
 
 
+          $sql = "SELECT * FROM trainer";
+          $result = mysqli_query($dbcon,$sql);
+          $queryResults = mysqli_num_rows($result);
+
+          if($queryResults>0){
+
+            echo "<div class='container'>
+              <div class='row container'>
+                <h5><p>We have found</p></h5>
+                <h5><p> &nbsp;".$queryResults."</p></h5>
+                <h5><p> &nbsp;results</p></h5>
+              </div>
+            </div>";
+
+              while($row= mysqli_fetch_assoc($result)){
+
+
+
+                echo "<div class='col-md-6 pt-3'>
+                  <div class='card flex-md-row mb-4 box-shadow h-md-250'>
+                          <img class='card-img-right flex-auto d-none d-md-block h-100' src='".$row['image']."' alt='Card image cap' width='160' />
+                          <div class='card-body d-flex flex-column align-items-start'>
+
+                              <h3 class='mb-0 container'>
+                              <div class='row'>
+                              <a class='text-dark'>".$row['fName']."</a>
+                              <a class='text-dark'>&nbsp;".$row['lName']."</a>
+                              </div>
+
+
+                              </h3>
+
+                          <div class='text-muted'>".$row['location']."</div>
+
+                          <div class='container'>
+                              <div class='row my-3'>
+                              <p> Experience : </p>
+                              <p> &nbsp; ".$row['experience']."</p>
+                              </div>
+
+                              <div class='row mb-auto'>
+                              <p>".$row['fee']."</p>
+                              <p> &nbsp;LKR per month</p>
+                              </div>
+                          </div>
+
+                          <div class='w-100'>
+
+                              <div class='row'>
+                                  <div class='col w-50'>
+                                  <a href='trainerBio.php?firstname=".$row['fName']."&lastname=".$row['lName']."' id='text-decoration-none'>View Profile</a>
+                                  </div>
+
+                                  <div class='col d-flex justify-content-end'>
+                                  *****
+                                  </div>
+
+                              </div>
+                          </div>
+
+                          </div>
+
+                      </div>
+
+                  </div>";
+
+
+                }
+          }
+
+
+
+    }
+
+
 
   }
 
 
 
-}
+
 
 
 
